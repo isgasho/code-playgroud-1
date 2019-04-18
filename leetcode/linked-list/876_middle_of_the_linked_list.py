5278f0a7b472e5a68b3869ede077a0d6fdc0c1ea
张博 <zhangbo@zaih.com>
2019-04-18 22:56:33 +0800

    876 py

---
+++ leetcode/linked-list/876_middle_of_the_linked_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        tmp = head
+        while tmp and tmp.next:
+            head = head.next
+            tmp = tmp.next.next
+        return head
